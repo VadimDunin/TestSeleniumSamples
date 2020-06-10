049d9e55bf04e8cf4c40ae27e119101bc0c8270e
Vadim <duninv@gmail.com>
2020-06-10 10:17:51 +0300

    def

--- emex/venv/Scripts/activate.bat
+++ emex/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\Vadim\PycharmProjects\emex\venv"
+set "VIRTUAL_ENV=E:\Vadim\Develop\TestSeleniumSamples\emex\venv"
 
 if not defined PROMPT (
     set "PROMPT=$P$G"
